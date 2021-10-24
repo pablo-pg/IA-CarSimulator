937fa4677e8003c07753752b639120e8efc2febd
Pablo <alu0101318318@ull.edu.es>
2021-10-24 15:00:19 +0100

    Estructura inicial de car.py

--- car.py
+++ car.py
@@ -8,7 +8,18 @@
 
 
 class Car:
-  def __init__(self):
+  def __init__(self, x_origin, y_origin, maps):
+    self.x_pos = x_origin
+    self.y_pos = y_origin
+    maps = maps
+
+  def move(self):
+    pass
+
+  def f(self):
+    return self.heuristic() + self.g()
+
+  def g(self):
     pass
 
   def heuristic(self):
